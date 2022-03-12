5f109fb3a1d2c138d6420dbb656766df2d9c9e7c
xiaowei <xiaowei@csdn.net>
2022-03-12 14:33:29 +0800

    first commit

---
+++ src/main/java/cn/codeerever/dnall/DnallApplication.java
@@ -0,0 +1,13 @@
+package cn.codeerever.dnall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codeerever/dnall/DnallApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codeerever.dnall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
