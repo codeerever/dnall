adf19112f7de5b7a44a204352f8daad31637555b
codeerever <xiaowei.s@outlook.com>
2022-03-12 15:18:38 +0800

    init

---
+++ dnall-portal/src/main/java/cn/codeerever/dnall/portal/DnallPortalApplication.java
@@ -0,0 +1,13 @@
+package cn.codeerever.dnall.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnallPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnallPortalApplication.class, args);
+    }
+
+}
